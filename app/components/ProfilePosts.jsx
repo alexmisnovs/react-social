8650a70a31696e2200edfc0233334b9667ac8e42
Alex Misnovs <aleksandrmisnov@gmail.com>
2021-01-19 19:21:51 +0000

    Added dynamic content to profile page, created new component ProfilePosts

---
+++ app/components/ProfilePosts.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Axios from "axios";
+
+function ProfilePosts() {
+  const { username } = useParams();
+  const [isLoading, setIsloading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await Axios.get(`/profile/${username}/posts`);
+        console.log(response.data);
+        setPosts(response.data);
+        setIsloading(false);
+      } catch (e) {
+        console.log("There was a problem, profile posts");
+      }
+    }
+    fetchPosts();
+  }, []);
+
+  if (isLoading) return <div>Loading..</div>;
+
+  return (
+    <div className="list-group">
+      {posts.map(post => {
+        const date = new Date(post.createdDate);
+        const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        return (
+          <Link to={`/post/${post._id}`} key={post._id} href="#" className="list-group-item list-group-item-action">
+            <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> <span className="text-muted small">on {dateFormatted} </span>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ProfilePosts;
